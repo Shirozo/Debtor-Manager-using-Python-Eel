4fa361df4c5504bf772b8dbc954d9dc960a98938
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-06-26 00:18:54 +0800

    need to add a holder for the elements for a proper animation

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+from io import StringIO
+if sys.stdout is None:
+    sys.stdout = StringIO()
+if sys.stderr is None:
+    sys.stderr = StringIO()
 import eel
 from os import mkdir
+from flask import session
 from cs50 import SQL
 from werkzeug.security import generate_password_hash, check_password_hash
-import json
 
 db = SQL("sqlite:///views/database/database.db")
 
@@ -16,5 +22,10 @@ def login(password):
         return True
     else:
         return False
+    
+@eel.expose
+def change_pass(new_password):
+    new_pass_hash = generate_password_hash(new_password)
+    db.execute("UPDATE exdafgf SET hyansasd = ?", new_pass_hash)
 
-eel.start("templates/login.html")
+eel.start("templates/login.html",)
--- views/static/script.js
+++ views/static/script.js
@@ -1,12 +1,61 @@
 async function logg(){
+    var error_code = document.getElementById("error_code");
     var password = document.getElementById("password").value;
     var status = await eel.login(password)();
-    if (status == "True"){
-        alert("True");
-        location.href = "templates/index.html";
+    if (status){
+        window.location.href = "../templates/index.html";
     }
     else{
-        var to_show = document.getElementsById("invalid");
-        alert("pass")
+        error_code.style.display = "inline-block";
     }
+}
+
+
+function show_change_pass(){
+    var show = document.getElementById("change_pass");
+    if (show.style.top === "-25%"){
+        show.style.display = "block";
+        show.style.top = "10%";
+    }
+    else{
+        show.style.top = "-25%";
+    }
+    validate();
+}
+
+async function validate(){
+    var NewPassword = document.getElementById("new_password");
+    var confirmation = document.getElementById("confirmation");
+    var changeBtn = document.getElementById("changeP");
+    changeBtn.disabled = true;
+    if (NewPassword.value.trim() === "" || confirmation.value.trim() === ""){
+        changeBtn.disabled = true;
+    }
+    else{
+        if (NewPassword.value.trim() != confirmation.value.trim()){
+            NewPassword.style.border = "red solid 1px";
+            NewPassword.style.outline = "red solid 1px";
+            confirmation.style.border = "red solid 1px";
+            confirmation.style.outline = "red solid 1px";
+            changeBtn.disabled = true;
+        }
+        else{
+            NewPassword.style.border = "green solid 1px";
+            NewPassword.style.outline = "green solid 1px";
+            confirmation.style.border = "green solid 1px";
+            confirmation.style.outline = "green solid 1px";
+            changeBtn.disabled = false;
+        }
+    }
+}
+
+async function passChange(){
+    var to_hide = document.getElementById("change_pass");
+    var success = document.getElementById("success");
+    var confirmed = document.getElementById("confirmation").value;
+    await eel.change_pass(confirmed);
+    to_hide.style.display = "none";
+    to_hide.style.top = "-25%"
+    success.style.display = "block";
+    success.style.top = "-25%"
 }
