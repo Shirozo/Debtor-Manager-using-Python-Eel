ab4bde42f735e5a3426efca0a1ef99bf701b31fa
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-06-28 23:27:11 +0800

    Function of adding data without reloading the page is done. Need to configure the DB next for each borrrower status

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ if sys.stdout is None:
 if sys.stderr is None:
     sys.stderr = StringIO()
 import eel
+import flask
 from cs50 import SQL
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 db = SQL("sqlite:///views/database/database.db")
-
 
 eel.init("views")
 
@@ -27,9 +28,34 @@ def change_pass(new_password):
     db.execute("UPDATE exdafgf SET hyansasd = ?", new_pass_hash)
 
 @eel.expose
-def fetch_data():
-    datas = db.execute("SELECT * FROM debt ORDER BY name")
+def fetch_data(order):
+    if order.lower() == "balance":
+        datas = db.execute("SELECT * FROM debt ORDER BY balance DESC")
+    elif order.lower() == "due date":
+        datas = db.execute("SELECT * FROM debt ORDER BY due_date")
+    else:
+        datas = db.execute("SELECT * FROM debt ORDER BY name")
     return datas
 
+
+@eel.expose
+def add_debt(name, amount, dueDate):
+    db.execute("INSERT INTO debt(name, loan, balance, due_date) VALUES(?, ?, ?, ?)", name.title(), amount, amount, dueDate)
+    return
+
+@eel.expose
+def date_checker(DateTime):
+    dateTime = DateTime.split("-")
+    if datetime(int(dateTime[0]), int(dateTime[1]), int(dateTime[2])) > datetime.now():
+        return True
+    return False
+
+@eel.expose
+def remover(idRM):
+    db.execute("PRAGMA foreign_keys = ON")
+    db.execute("DELETE FROM debt WHERE id = ?", idRM)
+    db.execute("PRAGMA foreign_keys = OFF")
+    return
+
 eel.start("templates/login.html",
-            disable_caache = True)
+            disable_cache = True)
--- views/static/script.js
+++ views/static/script.js
@@ -1,3 +1,96 @@
+// document.onkeydown = (e) => {
+//     if (e.ctrlKey && e.shiftKey && e.key == 'I' || e.ctrlKey && e.shiftKey && e.key == 'i') {
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.key == 'C'|| e.ctrlKey && e.shiftKey && e.key == 'c') {
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.key == 'J' || e.ctrlKey && e.shiftKey && e.key == 'j') {
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.key == 'U' || e.ctrlKey && e.key == 'u') {
+//         e.preventDefault();
+//     }
+// };
+
+let SORT_BY = "name";
+
+async function add_person(){
+    if (ensure_no_space() && ensure_number() && await ensure_date()){
+        var date = $("#due_date").val();
+        var loan = $("#loan_amount").val()
+        var name = $("#loan_name").val();
+        await eel.add_debt(name, loan, date);
+        $("#unsuccessful").css("display", "none")
+        $("#add_success").css("display", "inline-block");
+        await order_page(SORT_BY);
+        $("#debt_data")
+
+    }
+    else{
+        $("#unsuccessful").css("display", "inline-block")
+    }
+}
+
+function close_win(){
+    $("table").css({"-webkit-filter": "blur(0)", "filter" : "blur(0)"})
+    $("#add_loan").css("display", "none");
+    $("#loan_amount, #loan_name, #due_date").val("");
+    $("#loan_amount, #loan_name, #due_date").css({"border" : "black solid 1px", "outline" : "none"})
+}
+
+async function ensure_date(){
+    var datetime = $("#due_date").val();
+    if (datetime){
+        if(await eel.date_checker(datetime)()){
+            $("#due_date").css({"border" : "black solid 1px", "outline" : "none"})
+            return true
+        }
+        $("#due_date").css({"border" : "red solid 1px", "outline" : "red solid 1px"})
+        return false
+    }
+    $("#due_date").css({"border" : "red solid 1px", "outline" : "red solid 1px"})
+    return false
+}
+
+function ensure_no_space(){
+    var val = document.getElementById("loan_name");
+    if (val.value.trim() === ""){
+        $("#loan_name").css({"outline":"red solid 1px", "border":"solid red 1px"});
+        return false
+    }
+    else{
+        val.style = "revert";
+        return true
+    }
+}
+
+function ensure_number(){
+    var amount = $("#loan_amount").val()
+    if ($.isNumeric(amount)){
+        if (amount <= 0 ){
+            $("#loan_amount").css({"border" : "red solid 1px", "outline" : "red solid 1px"});
+            return false;
+        }
+        else{
+            $("#loan_amount").css({"border" : "black solid 1px", "outline" : "none"});
+            return true;
+        }
+    }
+    else{
+        $("#loan_amount").css({"border" : "red solid 1px", "outline" : "red solid 1px"});
+        return false;
+    }
+}
+
+function loan(){
+    var table = document.querySelector("table");
+    if (!(table.style.filter) || table.style.filter === "blur(0px)"){
+        $("#add_loan").css("display", "revert");
+        $("table").css({"-webkit-filter": "blur(6px)", "filter" : "blur(6px)"});
+    }
+}
+
 async function logg(){
     var password = document.getElementById("password").value;
     var status = await eel.login(password)();
@@ -9,6 +102,46 @@ async function logg(){
     }
 }
 
+async function order_page(event){
+    let inner = [];
+    try{
+        var order = event.srcElement.innerHTML;
+    }
+    catch(err){
+        var order = event;
+    }
+    SORT_BY = order;
+    var datas = await eel.fetch_data(order)();
+    for (data of datas){
+        var name = data.name.replace('<', '&lt;').replace('&', '&amp;');
+        var due_date = data.due_date.replace('<', '&lt;').replace('&', '&amp;');
+        inner+= '<tr>' + 
+                    '<td id="hasdhhj_v">' + name + '</td>' + 
+                    '<td>' + data.loan + '</td>'+
+                    '<td>' + data.balance + '</td>'+
+                    '<td>' + due_date + '</td>'+
+                    '<td class=actions>'+
+                        '<b onclick=lessen_debt()>ADD</b> | '+
+                        '<b onclick="remove('+ data.id +')">REMOVE</b>'+
+                    '</td>'+
+                '</tr>'
+    }
+    document.querySelector("tbody").innerHTML = inner;
+}
+
+async function passChange(){
+    var confirmed = document.getElementById("confirmation").value;
+    await eel.change_pass(confirmed);
+    $("#pass_data").css("display", "none");
+    $("#success").css("display", "block"); 
+    $("#change_pass").css("top", "-25%");
+}
+
+async function remove(id){
+    await eel.remover(id);
+    await order_page(SORT_BY);
+    $("#debt_data")
+}
 
 function show_change_pass(){
     $("#success").css("display", "none");
@@ -43,45 +176,4 @@ async function validate(){
             changeBtn.disabled = false;
         }
     }
-}
-
-async function passChange(){
-    var confirmed = document.getElementById("confirmation").value;
-    await eel.change_pass(confirmed);
-    $("#pass_data").css("display", "none");
-    $("#success").css("display", "block"); 
-    $("#change_pass").css("top", "-25%");
-}   
-
-function loan(){
-    var table = document.querySelector("table");
-    if (!(table.style.filter) || table.style.filter === "blur(0px)"){
-        $("#add_loan").css("display", "revert");
-        $("table").css({"-webkit-filter": "blur(6px)", "filter" : "blur(6px)"});
-    }
-}
-
-function close_win(){
-    $("table").css({"-webkit-filter": "blur(0)", "filter" : "blur(0)"})
-    $("#add_loan").css("display", "none");
-    $("#loan_amount").val("");
-    $("#loan_name").val("");
-}
-
-function ensure_no_space(){
-    var val = document.getElementById("loan_name");
-    if (val.value.trim() === ""){
-        $("#loan_name").css({"outline":"red solid 1px", "border":"solid red 1px"});
-    }
-    else{
-        val.style = "revert";
-    }
-}
-
-function ensure_number(){
-    alert($("#loan_amount").val())
-}
-
-async function add_person(){
-
 }
